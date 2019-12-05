7ce0f4156942ad430f6edb091e0f49740e095fe7
Bogdan <50374356+LoliScripter@users.noreply.github.com>
2019-12-06 00:50:22 +0300

    Update LabWork1_2.cpp

--- Lab_1/LabWork1_2.cpp
+++ Lab_1/LabWork1_2.cpp
@@ -4,11 +4,17 @@
 #include <math.h>
 #include <stdio.h>
 
+//	EXE 2
+/* Преобразование: 2D → 1D. Двумерный массив 5х3 вещественных чисел необходимо
+выложить в один ряд по элементам по столбцам сверху вниз.
+Инициализация: заполнить массив числами x[i][j] = sin(i – j) + cos(i + j).
+Вывод на экран : на каждый элемент массива отвести 10 позиций.
+*/
 
 void OutputArray(double* arr, size_t arr_size)
 {
-	for (int i = 0; i < arr_size; i++) std::cout << 
-    " | " << arr[i] ;
+	for (int i = 0; i < arr_size; i++) 
+		std::cout << " | " << arr[i] ;
 }
 
 void InitializeArray(double** arr, size_t size_i, size_t size_j)
@@ -18,11 +24,11 @@ void InitializeArray(double** arr, size_t size_i, size_t size_j)
         arr[i] = new double[size_j]; //one cell = new array
 
 	for(int i = 0; i < size_i; i++)
-        for(int j = 0; j < size_j; j++){
-            arr[i][j] = sin(i - j) + cos(i + j);
-            if(j < 2) std::cout << "|" << std::setw(10) << arr[i][j];
-            else if(j == 2) std::cout << "|" << std::setw(10) << arr[i][j] << std::endl;
-        }
+        	for(int j = 0; j < size_j; j++){
+            		arr[i][j] = sin(i - j) + cos(i + j);
+            		if(j < 2) std::cout << "|" << std::setw(10) << arr[i][j];
+            		else if(j == 2) std::cout << "|" << std::setw(10) << arr[i][j] << std::endl;
+        	}
 }
 
 double* Array_ToOneD(double** arr2D, double* arr1D, size_t size_i, size_t size_j)
@@ -46,7 +52,7 @@ int main()
 	int x = 5;
 	int y = 3;
     
-    double* arr = new double[x * y];
+    	double* arr = new double[x * y];
 	double** d_arr = new double*[x];
 	for(int i = 0; i < x; i++)
 		d_arr[i] = new double[y];
@@ -56,7 +62,7 @@ int main()
 
     //memory cleaning
 	for(int i = 0; i < x; i++)
-        delete[] d_arr[i];
+        	delete[] d_arr[i];
     delete[] d_arr;
     delete[] arr;
 }
